6736b63d69cf81caca151afb37a9cf6cae3f966a
lvinnichenko <lvinnichenko@aligntech.com>
2023-10-15 17:21:58 +0200

    adds initial training stand and api

---
+++ packages/training-stand/vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+};
