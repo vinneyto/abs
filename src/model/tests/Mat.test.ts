1c81f6f36d3a4e5f6ee96739dd653e722eb1a151
lvinnichenko <lvinnichenko@aligntech.com>
2023-11-06 23:44:49 +0100

    adds some tests

--- src/model/tests/Mat.test.ts
+++ src/model/tests/Mat.test.ts
@@ -13,4 +13,26 @@ describe('Testing Mat', () => {
 
     expect(mat.isDragging()).toBeTruthy();
   });
+
+  test('is not visible by default', () => {
+    expect(mat.isVisible()).toBeFalsy();
+  });
+
+  test('is visible after drag for a huge distance', () => {
+    expect(mat.isVisible()).toBeFalsy();
+
+    mat.startDrag(new Vector3());
+    mat.setControllerPosition(new Vector3(0, 0, 1));
+    mat.update();
+
+    expect(mat.isVisible()).toBeTruthy();
+  });
+
+  test('is invisible after drag and not moving', () => {
+    mat.startDrag(new Vector3());
+    mat.setControllerPosition(new Vector3(0, 0, 0));
+    mat.update();
+
+    expect(mat.isVisible()).toBeFalsy();
+  });
 });
