1de91a34ea25130ac289d930b92fddc405454dd5
vinneyto <vinneyto@gmail.com>
2023-12-08 00:07:18 +0100

    tests for MatModel

---
+++ packages/trainer/src/components/Mat/MatModel.test.ts
@@ -0,0 +1,38 @@
+import { Vector3 } from 'three';
+import { MatModel } from './MatModel';
+
+describe('Testing Mat', () => {
+  let mat: MatModel;
+
+  beforeEach(() => {
+    mat = new MatModel();
+  });
+
+  test('startDrag should enable dragging', () => {
+    mat.startDrag(new Vector3());
+
+    expect(mat.isDragging()).toBeTruthy();
+  });
+
+  test('is not visible by default', () => {
+    expect(mat.isVisible()).toBeFalsy();
+  });
+
+  test('is visible after drag for a huge distance', () => {
+    expect(mat.isVisible()).toBeFalsy();
+
+    mat.startDrag(new Vector3());
+    mat.setControllerPosition(new Vector3(0, 0, 1));
+    mat.update();
+
+    expect(mat.isVisible()).toBeTruthy();
+  });
+
+  test('is invisible after drag and not moving', () => {
+    mat.startDrag(new Vector3());
+    mat.setControllerPosition(new Vector3(0, 0, 0));
+    mat.update();
+
+    expect(mat.isVisible()).toBeFalsy();
+  });
+});
